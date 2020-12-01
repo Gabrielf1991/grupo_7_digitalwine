03524caf7107ddc0c5b3716177d9154e6446196e
Santi-GDS <sgarciadestefano@gmail.com>
2020-12-01 17:30:18 -0300

    creamos rutas y metodos de controladores

--- src/app.js
+++ src/app.js
@@ -4,15 +4,17 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+
 const usersRouter = require('./routes/users');
 const carritoRouter = require('./routes/carrito');
 const loginRouter = require('./routes/login');
 const productoRouter = require('./routes/product-detail');
 const registerRouter = require('./routes/register');
 const productCreateFormRouter = require('./routes/product-create-form');
-const productEditFormRouter = require('./routes/product-edit-form');
-const productosRouter = require('./routes/products-list');
+const productEditFormRouter = require('./routes/product-edit-form'); 
+
+const productsRouter = require('./routes/products');
+const mainRouter = require('./routes/main');
 
 const app = express();
 
@@ -27,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 
-app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 app.use('/carrito', carritoRouter);
 app.use('/login', loginRouter);
@@ -35,7 +37,9 @@ app.use('/product-detail', productoRouter);
 app.use('/register', registerRouter);
 app.use('/product-create-form', productCreateFormRouter);
 app.use('/product-edit-form', productEditFormRouter);
-app.use('/products-list', productosRouter);
+
+app.use('/products', productsRouter);
+app.use('/', mainRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -36,7 +36,67 @@ const controller = {
         res.render ('products/product-detail', {
             product: result
         })
+    },
+    
+    create: (req, res) => {
+
+        res.render('product-create-form');
+    },
+    
+    store: (req, res, next) => {
+
+        const newProduct = {
+			id: generateNewId(),
+			name:req.body.name,
+			price:req.body.price ,
+            description:req.body.description,
+            bodega: req.body.bodega,
+            // image: "" //
+
+		}
+		const products = getAllProducts();
+		const productsToSave = [...products, newProduct];
+		writeProducts(productsToSave);
+
+		res.redirect('/');
+
+    },
+    edit: (req, res, next) => {
+        const products = getAllProducts();
+		const id = req.params.id;
+		const result = products.find((product) => product.id == id)
+
+		res.render('product-edit-form', {
+			productToEdit : result
+		})
+
+        
+    },
+    update: (req, res, next) => {
+        const products =getAllProducts();
+		const id = req.params.id;
+		const newProduct = products.map((product) => { 
+
+			if(id == product.id){
+				product.name = req.body.name;
+				product.price = req.body.price;
+				product.description = req.body.description;
+				// product.image = re.files[0] ? req.files[0].filename : product.image; //
+			}
+
+			return product
+		})
+		writeProducts(newProduct);
+
+		res.redirect('/products/product-detail/' + id);
+
+        
+    },
+    destroy: (req, res, next) => {
+
+        
     }
+
 }
 
 module.exports = controller;
