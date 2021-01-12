afdb1b3d530d2a80c302b1e0c83cd53c52ae40eb
Santi-GDS <sgarciadestefano@gmail.com>
2021-01-12 15:27:28 -0300

    Subimos script de datos

--- data.sql
+++ data.sql
@@ -1,24 +1,118 @@
 /* TABLA DE PRODUCTOS */
 
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (1, 'Thornton', '$3871.86', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.', 'Gulgowski, Howe and Windler', 'Omba', 'http://dummyimage.com/215x207.png/dddddd/000000');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (2, 'Gaylord', '$6116.39', 'Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus. Phasellus in felis. Donec semper sapien a libero.', 'Heller-Lang', 'Aimbo', 'http://dummyimage.com/145x226.bmp/5fa2dd/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (3, 'Hobart', '$4212.61', 'Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.', 'Moen, Monahan and Frami', 'Wikizz', 'http://dummyimage.com/109x214.jpg/ff4444/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (4, 'Madella', '$1290.64', 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus.', 'Dickinson Inc', 'Zoombeat', 'http://dummyimage.com/106x176.png/5fa2dd/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (5, 'Aleen', '$4648.81', 'Vivamus tortor.', 'Muller, Orn and Dooley', 'Trunyx', 'http://dummyimage.com/208x138.png/cc0000/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (6, 'Gannie', '$6296.71', 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', 'Keeling and Sons', 'Voomm', 'http://dummyimage.com/200x187.png/ff4444/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (7, 'Ulberto', '$699.19', 'Nam nulla.', 'Deckow-Lubowitz', 'Mybuzz', 'http://dummyimage.com/220x195.png/cc0000/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (8, 'Archambault', '$7816.50', 'Proin at turpis a pede posuere nonummy.', 'Rutherford Inc', 'Meeveo', 'http://dummyimage.com/199x201.jpg/cc0000/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (9, 'Jakie', '$4989.82', 'Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.', 'Swaniawski LLC', 'Skyvu', 'http://dummyimage.com/140x195.png/cc0000/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (10, 'Florentia', '$8816.48', 'Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti.', 'Lemke Inc', 'Gabtype', 'http://dummyimage.com/197x250.jpg/ff4444/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (11, 'Riva', '$6849.86', 'In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy.', 'Gerhold-Crist', 'Fadeo', 'http://dummyimage.com/213x171.jpg/cc0000/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (12, 'Caldwell', '$2150.44', 'Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue.', 'Ruecker, Lockman and Wiegand', 'Aibox', 'http://dummyimage.com/133x182.bmp/cc0000/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (13, 'Kimbra', '$6797.20', 'Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo.', 'Kling LLC', 'Shuffledrive', 'http://dummyimage.com/182x221.png/5fa2dd/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (14, 'Catlin', '$1486.89', 'Sed vel enim sit amet nunc viverra dapibus.', 'Tremblay, Wilkinson and Wolff', 'Oozz', 'http://dummyimage.com/152x170.png/dddddd/000000');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (15, 'Trip', '$8171.12', 'Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula.', 'Hills-Zulauf', 'Quimba', 'http://dummyimage.com/159x191.bmp/dddddd/000000');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (16, 'Errick', '$9863.16', 'Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', 'Daniel, Schroeder and Denesik', 'Skyble', 'http://dummyimage.com/146x161.bmp/cc0000/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (17, 'Andree', '$184.83', 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum.', 'Batz, Morar and Maggio', 'Meevee', 'http://dummyimage.com/110x161.bmp/cc0000/ffffff');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (18, 'Raye', '$7853.35', 'Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 'Tromp Inc', 'Mymm', 'http://dummyimage.com/246x109.png/dddddd/000000');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (19, 'Dorene', '$2841.21', 'Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 'Rohan Group', 'Gabvine', 'http://dummyimage.com/107x216.jpg/dddddd/000000');
+
 insert into Products (id, name, price, detail, varietal, wine_cellar, image) values (20, 'Adlai', '$6161.92', 'Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus.', 'Powlowski and Sons', 'Npath', 'http://dummyimage.com/182x172.png/5fa2dd/ffffff');
 
 /* TABLA DE USUARIOS */
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (1, 'Wildon', 'Allix', 'wallix0@lulu.com', 'bfvQY22', '9/8/2001', 'https://robohash.org/rerumperspiciatisid.png?size=50x50&set=set1', 76);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (2, 'Hastings', 'Chatwin', 'hchatwin1@oaic.gov.au', 'IqCe9UMZP', '7/29/1980', 'https://robohash.org/etquiaut.png?size=50x50&set=set1', 19);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (3, 'Umberto', 'Hayhurst', 'uhayhurst2@discuz.net', '4U6GI6w', '9/10/1979', 'https://robohash.org/quiveroqui.bmp?size=50x50&set=set1', 6);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (4, 'Estell', 'Broadbent', 'ebroadbent3@tripod.com', 'gc6z046', '9/10/1948', 'https://robohash.org/velmagnamvoluptatem.bmp?size=50x50&set=set1', 99);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (5, 'Armstrong', 'Gee', 'agee4@macromedia.com', 'bpbhADE4', '10/28/1973', 'https://robohash.org/quioptiosit.jpg?size=50x50&set=set1', 44);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (6, 'Hartwell', 'Luparti', 'hluparti5@stumbleupon.com', 'DMgniwfju0or', '9/27/1969', 'https://robohash.org/cumquequisperspiciatis.jpg?size=50x50&set=set1', 75);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (7, 'Zach', 'Rylett', 'zrylett6@cloudflare.com', 'jXA3xGZw6', '6/24/1992', 'https://robohash.org/quiodiohic.bmp?size=50x50&set=set1', 2);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (8, 'Maryl', 'Guiduzzi', 'mguiduzzi7@sciencedaily.com', 'azgdlVdw4bc', '6/14/1991', 'https://robohash.org/doloribusminuseum.jpg?size=50x50&set=set1', 49);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (9, 'Tucky', 'Everill', 'teverill8@businesswire.com', 'uyadARZxvQ', '9/2/1973', 'https://robohash.org/nondelectusnumquam.jpg?size=50x50&set=set1', 25);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (10, 'Maurene', 'Dmiterko', 'mdmiterko9@prlog.org', 'T8Ayj7', '8/1/1999', 'https://robohash.org/cupiditateipsumquisquam.bmp?size=50x50&set=set1', 11);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (11, 'Monti', 'Coultous', 'mcoultousa@deviantart.com', 'ZjRW8rEW', '5/26/1954', 'https://robohash.org/maximetotamnihil.bmp?size=50x50&set=set1', 69);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (12, 'Quintina', 'Bellinger', 'qbellingerb@vk.com', 'VelL35P1eEvu', '12/7/1957', 'https://robohash.org/liberoautsaepe.png?size=50x50&set=set1', 87);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (13, 'Lida', 'Vellden', 'lvelldenc@squarespace.com', 'xM4SHom5v1Y', '11/27/1985', 'https://robohash.org/explicaboautmaxime.jpg?size=50x50&set=set1', 81);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (14, 'Corey', 'O''Kuddyhy', 'cokuddyhyd@nih.gov', 'LEqZNFvYWC', '6/1/1943', 'https://robohash.org/sintenimquibusdam.bmp?size=50x50&set=set1', 1);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (15, 'Oralle', 'Welch', 'owelche@webnode.com', 'nMSDvja', '11/30/1969', 'https://robohash.org/nostrumestdolores.bmp?size=50x50&set=set1', 91);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (16, 'Bird', 'Boddymead', 'bboddymeadf@networkadvertising.org', 'IQ5SAeCU1Eh', '6/13/1968', 'https://robohash.org/utquoquidem.jpg?size=50x50&set=set1', 68);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (17, 'Casie', 'Craiker', 'ccraikerg@paypal.com', 'Qo1vuqLkBH', '12/13/1957', 'https://robohash.org/essecumrepudiandae.bmp?size=50x50&set=set1', 51);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (18, 'Dimitri', 'Ickowics', 'dickowicsh@histats.com', 'RUG7hWyX3Qj', '1/23/1954', 'https://robohash.org/voluptasvoluptatemdistinctio.bmp?size=50x50&set=set1', 29);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (19, 'Darrick', 'Alejo', 'dalejoi@moonfruit.com', '7vfdTOmvIR', '12/30/1958', 'https://robohash.org/idcumquequis.png?size=50x50&set=set1', 43);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (20, 'Gage', 'Bullan', 'gbullanj@bizjournals.com', 'OSOerK', '7/29/1990', 'https://robohash.org/totamenimdolorem.bmp?size=50x50&set=set1', 2);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (21, 'Trish', 'Houlston', 'thoulstonk@apache.org', 'KQWpUv6gd', '12/19/1985', 'https://robohash.org/odititaquecorrupti.bmp?size=50x50&set=set1', 27);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (22, 'Abrahan', 'Bauckham', 'abauckhaml@irs.gov', 'iRBwvYfLB9O', '5/23/1970', 'https://robohash.org/oditodioamet.png?size=50x50&set=set1', 86);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (23, 'Britta', 'Wittrington', 'bwittringtonm@google.com.br', 'UjcKIJCA', '10/23/1994', 'https://robohash.org/enimrerumvoluptas.jpg?size=50x50&set=set1', 64);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (24, 'Winfred', 'Oneil', 'woneiln@alibaba.com', 'XL8F7cSSm565', '1/18/1946', 'https://robohash.org/exercitationemquisquamnon.bmp?size=50x50&set=set1', 24);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (25, 'Tandy', 'Giacoboni', 'tgiacobonio@mac.com', 'cx5OMzmGvV', '7/10/2000', 'https://robohash.org/facilisvoluptasrem.png?size=50x50&set=set1', 8);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (26, 'Selma', 'McNaught', 'smcnaughtp@hao123.com', 'r347hn', '6/4/1998', 'https://robohash.org/officiaiustoassumenda.png?size=50x50&set=set1', 96);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (27, 'Babs', 'Maisey', 'bmaiseyq@stumbleupon.com', 'AzZTi83', '4/24/1990', 'https://robohash.org/magnamquisexcepturi.jpg?size=50x50&set=set1', 46);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (28, 'Baillie', 'Brimming', 'bbrimmingr@jalbum.net', '8SB8RZ', '6/13/1964', 'https://robohash.org/eaitaqueimpedit.bmp?size=50x50&set=set1', 30);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (29, 'Estell', 'Pavlishchev', 'epavlishchevs@163.com', 'nyqwvKvFOi2', '11/19/1974', 'https://robohash.org/autarchitectosequi.jpg?size=50x50&set=set1', 85);
+
+insert into Users (id, name, last_name, email, password, DOB, avatar, rol) values (30, 'Ogdan', 'Anneslie', 'oannesliet@senate.gov', 'lCQuNW6nM8Fl', '8/22/1986', 'https://robohash.org/providentquasiblanditiis.jpg?size=50x50&set=set1', 28);
+
+/* TABLA DE CATEGORIAS */
+
+insert into Categories (id, name) values (1, 'Indocyanine green');
+insert into Categories (id, name) values (2, 'Slash Pine');
+insert into Categories (id, name) values (3, 'COTTONWOOD MIX, EASTERN/WESTERN POLLEN');
+insert into Categories (id, name) values (4, 'Dipyridamole');
+insert into Categories (id, name) values (5, 'PAROEX');
+insert into Categories (id, name) values (6, 'Gelato Topical Anesthetic');
+insert into Categories (id, name) values (7, 'DROSPIRENONE AND ETHINYL ESTRADIOL');
+insert into Categories (id, name) values (8, 'Daytime Sinus Relief');
+insert into Categories (id, name) values (9, 'Celazome Fade Shades Lightening Cream w/ Glycolic Acid');
+insert into Categories (id, name) values (10, 'Risperidone');
+
+
