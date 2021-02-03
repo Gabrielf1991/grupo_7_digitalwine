74446de5ec5feb1be73596fb46227fe1d6b13207
feercenteno <fer_95_08@hotmail.com>
2021-02-02 22:54:37 -0300

    termino validaciones en Front

--- public/js/script.js
+++ public/js/script.js
@@ -5,26 +5,82 @@ const email = document.querySelector("#email");
 const avatar = document.querySelector("#avatar");
 const password = document.querySelector("#password");
 const errorsElement = document.querySelector(".error");
+const small = document.querySelector(".smallError");
 
 form.addEventListener("submit", function(event) {
     
-    console.log("asdddddddd");
+    // console.log("asdddddddd");
 
-    const errors = [];
+    // const errors = [];
 
-    errorsElement.innerHTML = ''
+    // errorsElement.innerHTML = ''
 
-    if (name.value.trim().length <= 2) {
-        errors.push('Complete el nombre')
-    }
+    // if (name.value.trim().length <= 2) {
+    //     //errors.push('Complete el nombre')
+    //     small.innerHTML = ('Complete el nombre')
+    // }
 
-    if (errors.length) {
-        for(const error of errors) {
-            errorsElement.innerHTML += `<li>${error}</li>` 
-        }
-        event.preventDefault();
-    }
+    // if (errors.length) {
+    //     for(const error of errors) {
+    //         errorsElement.innerHTML += `<li>${error}</li>` 
+    //     }
+    //     event.preventDefault();
+    // }
 
-
+    event.preventDefault();
+    checkInputs();
     
 })
+
+function checkInputs (){
+    
+    const nameValue = (name.value.trim().length >= 2) ;
+    const last_nameValue = (last_name.value.trim().length >= 2);
+    const emailValue = (email.value.trim());
+    const passwordValue = (password.value.trim().length >= 8);
+
+    if (nameValue == ''){
+        setErrorFor(name, "El nombre debe tener al menos 2 caracteres")
+    } else {
+        setSuccesFor(name);
+    }
+
+    if (last_nameValue == ''){
+        setErrorFor(last_name, "El apellido debe tener al menos 2 caracteres")
+    } else {
+        setSuccesFor(last_name);
+    }
+
+    if (emailValue == ''){
+        setErrorFor(email, "Debe ingresar un email");
+    } else if (isEmail(emailValue)){
+        setErrorFor(email, 'No ingresó un email válido');
+    } else {
+        setSuccesFor(email);
+    }
+
+    if (passwordValue == ''){
+        setErrorFor(password, "La contraseña debe tener al menos 8 caracteres")
+    } else {
+        setSuccesFor(password);
+    }
+
+
+}
+
+function isEmail(email) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email);
+  }
+
+function setErrorFor(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('.smallError');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccesFor(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
