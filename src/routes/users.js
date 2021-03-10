eafbfa1693a499d4adad0e01e0f6df9a95cbf26b
Santi-GDS <sgarciadestefano@gmail.com>
2021-03-10 12:31:45 -0300

    agregamos view de los blogs y su contenido

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,18 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
 const usersController = require('../controllers/usersController');
 const authMiddleware = require('../middlewares/authMiddleware');
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/tmp/my-uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-})
-const upload = multer({ storage: storage })
 
 
 router.get('/profile', authMiddleware, usersController.showProfile)
